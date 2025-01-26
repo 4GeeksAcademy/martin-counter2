4685e1e15217bdcadd479c18e3ad6cd439c02f9c
martinSabatini <146122086+martinSabatini@users.noreply.github.com>
2025-01-26 13:56:44 +0000

    SecondsCounter2

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
 
 //Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,9 +11,31 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
+import SecondsCounter from './components/Seconds.Counter';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+let secondsPassed = 0;
+
+const renderCounter = () => {
+    secondsPassed++;
+    root.render(
+        <React.StrictMode>
+            <ParentComponent secondsPassed={secondsPassed} />
+        </React.StrictMode>
+    );
+};
+
+const ParentComponent = (props) => {
+    return (
+        <div className="top-banner">
+            
+            <SecondsCounter secondsPassed={props.secondsPassed} />
+        </div>
+    );
+};
+
+window.addEventListener("load", () => {
+    setInterval(renderCounter, 1000);
+});
